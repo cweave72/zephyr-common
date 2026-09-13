70ed316111e8567d5865b2b713b767925260b89d
Craig <cdweave@gmail.com>
2026-09-13 09:53:15 -0400

    Permit more than one proto search path in nanopb_build_sources
    nanopb_build_sources accepted one base_path and always created a library
    named <target>_proto. An application that has its own proto directory must
    also use the workspace proto directory. Two calls to the function created
    the library two times, thus CMake stopped with an add_library error.
    
    The function now accepts one or more search paths through ARGN. It collects
    the proto files from all paths. Then it creates the library and generates
    the sources one time.
    
    The glob now also matches <path>/<Name>.proto. Before this change it matched
    only <path>/<Name>/<Name>.proto, which is the workspace layout. Thus an
    application can put a single proto file directly in its proto directory.
    
    Add the generated header directory to the include path. nanopb writes the
    headers to a subdirectory of the binary directory when the proto file is in
    the application source tree. Thus the compiler did not find these headers.
    
    Delete the protoc_include_paths and proto_file_stems code. The function
    computed both values but did not use them.
    
    Existing callers supply one path. They do not change.

--- scripts/cmake/build_proto.cmake
+++ scripts/cmake/build_proto.cmake
@@ -1,13 +1,10 @@
-function(nanopb_build_sources target base_path)
+function(nanopb_build_sources target)
 
     list(APPEND CMAKE_MODULE_PATH ${ZEPHYR_BASE}/modules/nanopb)
     include(nanopb)
 
-    message(STATUS "[nanopb] Building protos into library: ${target}_proto")
-    message(STATUS "[nanopb] Searching for protos from path: ${base_path}")
-
-    # Create a library for all nanopb source.
     set(libname ${target}_proto)
+    message(STATUS "[nanopb] Building protos into library: ${libname}")
     zephyr_library_named(${libname})
 
     # Reference: Setting options for protoc and nanopb plugin.
@@ -17,27 +14,33 @@ function(nanopb_build_sources target base_path)
     #    )
     #set(PROTOC_OPTIONS --help)
 
-    # Gather all .proto files.
-    FILE(GLOB protofiles ${base_path}/*/*.proto)
+    # Find all .proto files in each search path. A search path can use two
+    # layouts: <path>/<Name>/<Name>.proto or <path>/<Name>.proto.
+    set(protofiles)
+    foreach(base_path ${ARGN})
+        message(STATUS "[nanopb] Searching for protos from path: ${base_path}")
+        FILE(GLOB found ${base_path}/*/*.proto ${base_path}/*.proto)
+        list(APPEND protofiles ${found})
 
-    # Construct include paths based on where output files (*.pb.h) will be
-    # located. 
-    #set(header_include_paths)
-    set(protoc_include_paths)
-    set(proto_file_stems)
-    foreach(p ${protofiles})
-        cmake_path(GET p STEM stem)
-        #message(stem=${stem})
-        list(APPEND protoc_include_paths "-I${CMAKE_CURRENT_SOURCE_DIR}/${stem}")
-        #list(APPEND header_include_paths ${CMAKE_CURRENT_BINARY_DIR}/${stem})
-        list(APPEND proto_file_stems ${stem})
+        # nanopb writes headers to a subdirectory of the binary directory.
+        # The subdirectory has the same path as the proto file relative to
+        # CMAKE_CURRENT_SOURCE_DIR. This occurs when the proto file is in
+        # the application source tree. Refer to NANOPB_GENERATE_CPP_RELPATH
+        # in FindNanopb.cmake.
+        # Example: an application proto/ directory generates headers in
+        # ${CMAKE_CURRENT_BINARY_DIR}/proto.
+        # Add this subdirectory to the include path. If you do not add it,
+        # the compiler cannot find <name>.pb.h.
+        file(RELATIVE_PATH base_path_rel ${CMAKE_CURRENT_SOURCE_DIR} ${base_path})
+        if(NOT base_path_rel MATCHES "^\\.\\.")
+            zephyr_include_directories(${CMAKE_CURRENT_BINARY_DIR}/${base_path_rel})
+        endif()
     endforeach()
 
     # Must remove nanopb.proto from the list.
     list(FILTER protofiles EXCLUDE REGEX ".*nanopb.proto")
 
     message(STATUS "[nanopb] Found proto files: ${protofiles}")
-    #message(STATUS "[nanopb] Setting include paths: ${header_include_paths}")
 
     zephyr_include_directories(${CMAKE_CURRENT_BINARY_DIR})
 
